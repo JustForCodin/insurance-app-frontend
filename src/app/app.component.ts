32b4afb13dc499f78417e1f84df5e39c9e50d8d5
JustForCodin <musicjohnliofficial@gmail.com>
2025-02-21 21:38:18 +0200

    modified main app component + auth/routing setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AuthService } from './services/auth.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'insurance-app-frontend';
+  isCheckingAuth = true;
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.authService.getCurrentUser().subscribe({
+      next: (response) => {
+        this.isCheckingAuth = false;
+        this.router.navigate(['/branches'])
+      },
+      error: (error) => {
+        this.isCheckingAuth = false;
+        this.router.navigate(['/login']);
+      }
+    });
+  }
 }
